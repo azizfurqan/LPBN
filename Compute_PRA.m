5942f285bc9778a8cd7b7e8438c55d3605362d05
azizfurqan <48009402+azizfurqan@users.noreply.github.com>
2021-03-30 12:25:07 +0100

    Update Compute_PRA.m

--- Compute_PRA.m
+++ Compute_PRA.m
@@ -1,7 +1,6 @@
 function [predict_links,scores] = Compute_PRA(A)
 
-
-%Code to compute Path-based Resource Allocation (PRA) index for Bipartite networks
+%For upload on Github
 
 %A is the adjacency matrix
 %score: Path-based RA scores
@@ -16,30 +15,15 @@ num_lnks = size(predict_links,1); % number of links to be predicted
 % Initialize variables
 scores=zeros(num_lnks,1);
 
-% Empty cells for storing neighbours of row/col nodes
-ne_r=cell(size(A,2),1); 
-ne_c=cell(size(A,1),1); 
 
-% compute neighbours (only for nodes in links)
-nodes_r=unique(predict_links(:,1));
-nodes_c=unique(predict_links(:,2));
 
-parfor i=1:size(A,1)
-    if any(i==nodes_r)
-        ne_c{i}=find(A(i,:));
-    end
-end
-
-parfor i=1:size(A,2)
-    if any(i==nodes_c)
-        ne_r{i}=find(A(:,i));
-    end
-end
+% Neighbours list for each row and column
+ne_r = cellfun(@(x) find(x),num2cell(A,1),'un',0);
+ne_r = ne_r';
+ne_c = cellfun(@(x) find(x),num2cell(A,2),'un',0);
 
 deg_r_all=sum(A,2); % Degrees of all the row nodes
 deg_c_all=sum(A,1); % Degrees of all the column nodes
-
-
 
 
 parfor i=1:num_lnks
@@ -47,15 +31,12 @@ parfor i=1:num_lnks
     % Take submatrix of neighbours of nodes involved in the link
     n_r=ne_r{predict_links(i,2)};
     n_c=ne_c{predict_links(i,1)};
-    part_mat=A(n_r,n_c);
+    sub_mat=A(n_r,n_c);
     
-%     deg_r=deg_r_all(predict_links(i,1));
-%     deg_c=deg_c_all(predict_links(i,2));
-    
-%     scores(i) = sum(sum(((1./deg_r(n_r))*(1./deg_c(n_c))).*part_mat));
-    part_mat = ((deg_r_all(n_r))*deg_c_all(n_c)).*part_mat;
-    part_mat = 1./part_mat;
-    part_mat(isinf(part_mat))=0;
-    scores(i) = sum(sum(part_mat));
+
+    sub_mat = ((deg_r_all(n_r))*deg_c_all(n_c)).*sub_mat;
+    sub_mat = 1./sub_mat;
+    sub_mat(isinf(sub_mat))=0;
+    scores(i) = sum(sum(sub_mat));
     
 end
